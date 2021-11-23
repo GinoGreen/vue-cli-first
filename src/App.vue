71fbbc5f34288aa12c4d71778016a1f92e54805c
GinoGreen <luigiverde97@gmail.com>
2021-11-23 15:26:10 +0100

    compilazione errata

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <template>
-  <div>
-    
+  <div id="app">
+    <Header/>
+    <Main/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
+    Header,
+    Main,
+    Footer
+    
   }
 }
 </script>
 
 <style>
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    height: 100vh;
+    padding: 10px;
+  }
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+     <h1></h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+   name: 'Header'
+}
+</script>
+
+<style scoped>
+   header {
+      height: 80px;
+      width: 100%;
+   }
+   h1 {
+      text-align: center;
+   }
+</style>
